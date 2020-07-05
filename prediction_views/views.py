2b6ffc9101c108f146beb9ab96537a4164869db6
kyunghankim <ilikeorange@nate.com>
2020-07-05 18:21:36 +0000

    testforlast

--- prediction_views/views.py
+++ prediction_views/views.py
@@ -47,7 +47,7 @@ def detail(request, pk):
     context = {
         'article': article,
         'comment_form': comment_form,
-        'image': image,
+        # 'image': image,
     }
     return render(request, 'prediction_views/detail.html', context)
 
